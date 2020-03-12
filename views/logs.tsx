5496b2757ac111ead8feaf39229dc1ec499829fb
Troy Goode <troygoode@gmail.com>
2020-03-11 18:50:34 -0700

    logs design

--- views/logs.tsx
+++ views/logs.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
-import { Platform, StatusBar, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Image, Platform, StatusBar, StyleSheet, Text, View } from 'react-native';
 import { IGlobalState } from '../lib/use-global-state';
 import ShareIcon from '../components/share';
 
-const FAKE_JSON = {
-  origin: 'selected',
-  data: {
-    example: 'test1',
-    isCool: true,
-    someNum: 42
-  },
-  remote: true
-};
+import watermarkSrc from '../assets/watermark/watermark.png';
+import robotSrc from '../assets/robot/robot.png';
 
 interface ILogsProps {
   state: IGlobalState;
 }
 
 const format = (text: string): string => {
-  const json = text ? JSON.parse(text) : FAKE_JSON; // TODO
+  const json = JSON.parse(text);
   return JSON.stringify(json, null, '  ');
 }
 
@@ -28,15 +21,29 @@ const Logs: React.FunctionComponent<ILogsProps> = ({ state }) => {
   return (
     <View style={styles.fullscreen}>
       <StatusBar barStyle="light-content" />
-      <ShareIcon background='dark' text={log} style={styles.shareIcon} />
-      <Text style={styles.h1}>Most Recent Notification</Text>
-      <Text style={styles.json}>{log}</Text>
+
+      {state.logEntry ? <View style={styles.absolute}>
+        <ShareIcon background='dark' text={log} style={styles.shareIcon} />
+        <Text style={styles.h1}>Most Recent Notification</Text>
+        <Text style={styles.json}>{log}</Text>
+      </View> : null}
+
+      {!state.logEntry ? <View style={styles.absolute}>
+        <Image source={watermarkSrc} style={styles.watermark} resizeMode='cover' />
+        <Text style={styles.robotSpeech}>Send a notification to your token via Courier and click it to see the log here!</Text>
+        <Image source={robotSrc} style={styles.robot} />
+      </View> : null}
     </View>
   );
-}
+};
 
 const monospace = Platform.OS === 'ios' ? 'Courier' : 'monospace';
 const styles = StyleSheet.create({
+  absolute: {
+    position: 'absolute',
+    width: '100%',
+    height: '100%'
+  },
   fullscreen: {
     backgroundColor: '#24324B',
     width: '100%',
@@ -48,6 +55,33 @@ const styles = StyleSheet.create({
     top: 45,
     right: 10,
     zIndex: 99
+  },
+
+  watermark: {
+    position: 'absolute',
+    height: '60%',
+    top: 150,
+    left: -15,
+  },
+  robotSpeech: {
+    // backgroundColor: 'rgba(200, 0, 0, .5)',
+    backgroundColor: '#fff',
+    position: 'absolute',
+    zIndex: 98,
+    fontFamily: monospace,
+    bottom: 233,
+    marginLeft: 79,
+    marginRight: 100,
+    width: '61%',
+    minHeight: 63,
+    borderRadius: 10,
+    overflow: 'hidden',
+    padding: 13
+  },
+  robot: {
+    position: 'absolute',
+    bottom: 100,
+    right: -10,
   },
 
   h1: {
